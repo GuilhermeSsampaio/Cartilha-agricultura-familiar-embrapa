bf37ad5383398f7392007aa0df0f69fb93f15447
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2023-09-26 10:48:50 -0400

    e funcionando email

--- back/config/plugins.js
+++ back/config/plugins.js
@@ -1,34 +1,35 @@
 module.exports = ({env}) => ({
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          show:{},
-          delete: {},
-        },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        show:{},
+        delete: {},
       },
     },
-    email: {
-      config: {
-        provider: 'nodemailer',
-        providerOptions: {
-          host: env('SMTP_HOST', 'smtp.gmail.com'),
-          port: env('SMTP_PORT', 587),
-          auth: {
-            user: env('guilhermessampaio20@gmail.com'),
-            pass: env('SMTP_PASSWORD'),
-          },
+  },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
         },
-        settings: {
-          defaultFrom: 'hello@example.com',
-          defaultReplyTo: 'hello@example.com',
-        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'no-reply@strapi.io',
+        defaultReplyTo: 'no-reply@strapi.io',
       },
     },
+  },
 })
