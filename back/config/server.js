0a63dd58d8c280fbad19ed80cac3b960043e6ae4
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2023-12-22 15:39:23 -0400

    td funcionando, falta o pwa no docker

--- back/config/server.js
+++ back/config/server.js
@@ -2,7 +2,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://www.cartilha-dev.com.br/cartilha/strapi',
+  url: 'http://www.cartilha-dev.com.br/strapi',
   app: {
     keys: env.array('APP_KEYS'),
   },
